f2f55019ca50f2636150ba08c670a39dac46f80c
sfwnisme <safymo424@gmail.com>
2025-07-02 14:24:07 +0300

    refactor(media-list-v2): refactor imports and styles

--- components/media-list-v2/MediaListV2.tsx
+++ components/media-list-v2/MediaListV2.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { IMedia } from '@/types'
 import React from 'react'
-import MediaCardV2 from '../mediCardV2/MediaCardV2'
+import MediaCardV2 from '../media-card-v2/MediaCardV2'
 
 type Props = {
   media: IMedia[]
@@ -12,7 +12,7 @@ export default function MediaListV2({ media, title }: Props) {
   return (
     <div>
       <h1 className="text-base p-4 font-bold border-b border-slate-600 pb-4 mb-1">{title}</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 p-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 gap-1 p-4'>
         {media.map((item) => (
           <MediaCardV2 key={item.trackId} media={item} />
         ))}
