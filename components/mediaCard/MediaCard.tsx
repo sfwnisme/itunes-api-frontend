fdc5b52620539ea195469af591f5468e8eb264e5
sfwnisme <safymo424@gmail.com>
2025-07-02 13:42:24 +0300

    refactor(MediaCard): added mediaworkUrl600 image and convert card title to anchor

--- components/mediaCard/MediaCard.tsx
+++ components/mediaCard/MediaCard.tsx
@@ -12,10 +12,10 @@ type Props = {
 export default function MediaCard({ media }: Props) {
   return (
     <div className="block h-full">
-      <Image src={media.artworkUrl100 || media.artworkUrl60 || media.artworkUrl30 || "/lazy-image.png"} alt="logo" width={230} height={230} className='mb-2 min-w-[230px] w-full rounded-md object-cover' />
+      <Image src={media.artworkUrl600 || media.artworkUrl60 || media.artworkUrl30 || "/lazy-image.png"} alt="logo" width={230} height={230} className='mb-2 min-w-[230px] w-full rounded-md object-cover' />
       <div className='flex justify-between items-start'>
         <div>
-          <h2 className='text-base font-medium mb-1'>{truncate(media.trackName, 100)}</h2>
+          <a href={media.trackViewUrl} className='text-base font-medium mb-1 hover:underline'>{truncate(media.trackName, 100)}</a>
           <p className='text-sm font-medium text-slate-400'>{truncate(media.artistName, 30)}</p>
         </div>
         <div className=''>
