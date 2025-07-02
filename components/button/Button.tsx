8c840f60dacbdd3431410556ea1deebce31c0525
sfwnisme <safymo424@gmail.com>
2025-07-02 13:40:44 +0300

    refactor(Button): remove ghost variant background

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function Button({ children, type = 'button', variant = 'ghost', ...props }: Props) {
   const variants: Record<string, string> = {
     default: " cursor-pointer bg-transparent text-slate-400 hover:text-slate-50 p-1 rounded-sm hover:bg-slate-700 transition-all duration-300",
-    ghost: " cursor-pointer bg-transparent text-slate-400 hover:text-slate-50 p-1 rounded-sm hover:bg-slate-800/50 transition-all duration-300",
+    ghost: " cursor-pointer bg-transparent text-slate-500 hover:text-slate-50 p-1 rounded-sm transition-all duration-300",
   }
   return (
     <button type={type} className={props.className + ' ' + variants[variant]} {...props}>{children}</button>
